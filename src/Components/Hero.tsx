bf078c5d8fada26959a6cebcee71b5e7871fa187
simonkadili47 <simonkadili47@gmail.com>
2024-09-13 16:21:01 +0100

    animate

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 
 const Hero: React.FC = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const heroRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect(); // Stop observing once the element is visible
+        }
+      },
+      {
+        threshold: 0.1, // Adjust based on when you want the animation to trigger
+      }
+    );
+
+    if (heroRef.current) {
+      observer.observe(heroRef.current);
+    }
+
+    return () => {
+      if (heroRef.current) {
+        observer.unobserve(heroRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div className="justify-between">
-      <div className=" flex flex-col md:flex-row">
-        <div className="text-5xl font-bold ml-24">
+      <div className="flex flex-col md:flex-row">
+        <div
+          ref={heroRef}
+          className={`text-5xl font-bold ml-24 ${
+            isVisible ? 'animate-fadeInDown' : ''
+          }`}
+        >
+          {/* Apply fade-in-down animation class conditionally */}
           <p>Taking Care of What's</p>
           <p>Important.</p>
         </div>
@@ -17,10 +50,8 @@ const Hero: React.FC = () => {
           <div className="mt-6 flex ">
             <Button
               label="Start free trial"
-              
-              className="text-white px-6 bg-black  py-2 border border-black"
+              className="text-white px-6 bg-black py-2 border border-black"
             />
-
             <Button
               label="Our Study Case"
               className="text-black px-6 py-2 ml-4 border border-black"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,7 +11,18 @@ export default {
       colors: {
         'custom-gray': '#e0ebe5',  
       },
+      keyframes: {
+        fadeInDown: {
+          '0%': { opacity: 0, transform: 'translateY(-20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeInDown: 'fadeInDown 1s ease-out',
+      },
     },
   },
   plugins: [],
 }
+
+
