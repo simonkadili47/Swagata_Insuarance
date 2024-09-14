c291795e2a8f1be3536e60a6a601dccf9d370754
simonkadili47 <simonkadili47@gmail.com>
2024-09-14 18:22:51 +0100

    border and paragraphs

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -64,12 +64,9 @@ const Hero: React.FC = () => {
       <div className="absolute top-[calc(100%)] left-0 right-0 grid grid-cols-1 lg:grid-cols-2 gap-8 justify-center mt-20">
         <div className="rounded-xl bg-white flex flex-col shadow-lg">
           <div className="p-4">
-            <h2 className="font-semibold text-lg pb-2">
-              Individuals and Family
-            </h2>
-            <p className="text-sm text-gray-500 pb-6">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic
-              harum aut sed enim! Quisquam, officiis. Lorem ipsum dolor sit amet.
+            <h2 className="font-semibold text-lg pb-2">Individuals and Family</h2>
+            <p className="text-md text-gray-500 pb-6">
+              If you die, we give you a proper send-off and help your family keep living their best life.
             </p>
           </div>
           <img
@@ -83,21 +80,49 @@ const Hero: React.FC = () => {
 
         <div className="rounded-xl bg-white flex flex-col shadow-lg">
           <div className="p-4">
-            <h2 className="font-semibold text-lg pb-2">
-              Business Protection
-            </h2>
-            <p className="text-sm text-gray-500 pb-6">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic
-              harum aut sed enim! Quisquam, officiis. Lorem ipsum dolor sit amet.
+            <h2 className="font-semibold text-lg pb-2">Business Protection</h2>
+            <p className="text-md text-gray-500 pb-6">
+              Protecting your business means staying one step ahead and helping you prevent problems before they happen.
             </p>
           </div>
           <img
             src={image1}
             width={360}
             height={360}
-            alt="Image1"
+            alt="Image2"
             className="w-full h-[360px] rounded-bl-xl rounded-br-xl object-cover"
           />
+        </div>
+
+       {/* Border line after images */}
+<div className="col-span-2 flex justify-between items-center mt-2">
+  <div className="border-t border-gray-200 w-full mx-8"></div>
+</div>
+
+
+        
+        <div className="mt-2 lg:col-start-1 lg:col-span-2 grid grid-cols-3 gap-6">
+          <div className="p-4">
+            <div className="text-black font-bold text-3xl">
+             <p>Simpler, smarter, more</p> 
+             <p>rewarding cover.</p>
+            </div>
+          </div>
+
+          <div className="p-4">
+            <div className="text-gray-700">
+              <span className="text-black text-xl font-bold"> <p>We help make sure you have no gaps </p></span>
+              <p className="text-sm mt-1">Wit Swagata, you get comprehensive, not patchwork protection</p>
+             
+            </div>
+          </div>
+
+          <div className="p-4">
+            <div className="text-gray-700">
+              <span className="text-xl text-black font-bold"><p>We look out for you.</p></span>
+              <p className="text-sm mt-1">You get a proactive patner  to help prevent issues form happening in the first place</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
