3dc136965f0788cca43904e628bde853655ed84a
kazimoto5520 <meshackkazimoto@gmail.com>
2024-09-18 21:53:04 +0300

    footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,68 +6,84 @@ import { FaArrowRightLong } from "react-icons/fa6";
 const Footer: React.FC = () => {
   return (
     <div className="bg-custom-blue">
-      <div className="flex ml-24 text-white pt-12">
-        <p className="text-zinc-400">Who we are</p>
-        <div className="ml-16 font-bold">
-          <p>Auto insuarance</p>
-          <p>Valuables</p>
-          <p>Liablity</p>
-          <p>Risk Engineering</p>
-          <p>Construction Safety</p>
-          <p>Industrial Hygine</p>
-        </div>
+      <div className="container mx-auto py-12">
+        <div className="flex justify-between text-white max-sm:flex-col max-sm:justify-between max-sm:items-start max-sm:gap-8 max-sm:px-8">
+          <div className="flex gap-8 max-sm:flex-col max-sm:gap-2">
+            <p className="text-zinc-400">Who we are</p>
+            <div className="font-bold">
+              <p>Auto insuarance</p>
+              <p>Valuables</p>
+              <p>Liablity</p>
+              <p>Risk Engineering</p>
+              <p>Construction Safety</p>
+              <p>Industrial Hygine</p>
+            </div>
+          </div>
 
-        <p className="text-zinc-400 ml-36">Campany</p>
+          <div className="flex flex-col gap-4">
+            <div className="flex gap-8 max-sm:flex-col max-sm:gap-2">
+              <p className="text-zinc-400">Campany</p>
+              <div className="font-bold">
+                <p>About Swagita</p>
+                <p>Login Swepartnet</p>
+                <p>Career</p>
+              </div>
+            </div>
 
-        <div className="ml-16 font-bold">
-          <p>About Swagita</p>
-          <p>Login Swepartnet</p>
-          <p>Career</p>
-        </div>
-        <p className="ml-32 text-zinc-400">Socials</p>
-        <div className="ml-16 font-bold">
-          <p>Facebook </p>
-          <p>Instagram</p>
-          <p>Twitter</p>
-          <p>Youtube</p>
-          <p>Linkedin</p>
-        </div>
-      </div>
-      <div className="flex">
-        <p className="text-zinc-400 ml-526">About Us</p>
-        <div className="text-white ml-20 font-bold">
-          <p>Contact us</p>
-          <p>Our Story</p>
+            <div className="flex gap-8 max-sm:flex-col max-sm:gap-2">
+              <p className="text-zinc-400">About Us</p>
+              <div className="text-white font-bold">
+                <p>Contact us</p>
+                <p>Our Story</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-8 max-sm:flex-col max-sm:gap-2">
+            <p className="text-zinc-400">Socials</p>
+            <div className="font-bold">
+              <p>Facebook </p>
+              <p>Instagram</p>
+              <p>Twitter</p>
+              <p>Youtube</p>
+              <p>Linkedin</p>
+            </div>
+          </div>
         </div>
       </div>
 
       <div className="border-t mt-3 border-gray-300"></div>
 
-      <div className="flex text-white ml-32 mt-6 justify-between">
-        <div className="flex">
-          <BsUmbrella className="text-4xl" />
-          <p className="text-4xl ml-3">Swagata</p>
-        </div>
-        <div className="font-bold mr-16">
-          <p>Stay informed with our newsletter</p>
-        </div>
-      </div>
+      <div className="container mx-auto py-8">
+        <div className="flex justify-between items-center text-white max-sm:flex-col max-sm:gap-8 max-sm:items-start max-sm:px-8">
+          <div className="flex flex-col gap-4">
+            <div className="flex">
+              <BsUmbrella className="text-4xl" />
+              <p className="text-4xl ml-3">Swagata</p>
+            </div>
 
-      <div className="justify-between ml-32 mt-4 ">
-        <div className="flex text-zinc-400 pb-3">
-        <FaRegCopyright className="text-md mt-1" />
-            <p className="ml-2">2022 Swagata.</p>
-            <p className="ml-1">All right reserved.</p>
-            <p className="ml-5">Privacy Policy</p>
-            <p className="ml-6">Terms and Conditions</p>
-            
-        <p className="ml-96">Type your email address</p>
-        <FaArrowRightLong className="mt-2 ml-16" />
-      
+            <div className="flex gap-4 text-gray-500 max-sm:gap-2 max-sm:flex-col">
+              <div className="flex items-center">
+                <FaRegCopyright className="text-md" />
+                <p className="hover:cursor-pointer">2022 Swagata.</p>
+              </div>
+              <p className="hover:cursor-pointer">All right reserved.</p>
+              <p className="hover:cursor-pointer">Privacy Policy</p>
+              <p className="hover:cursor-pointer">Terms and Conditions</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 border-b border-gray-500 max-sm:w-full">
+            <div className="font-bold">
+              <p>Stay informed with our newsletter</p>
+            </div>
+            <div className="flex justify-between items-center text-gray-500 pb-3">
+              <p className="">Type your email address</p>
+              <FaArrowRightLong className="" />
+            </div>
+          </div>
         </div>
-        
       </div>
-      
     </div>
   );
 };
